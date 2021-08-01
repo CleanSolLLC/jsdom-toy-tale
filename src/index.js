579ff1feb38b6c0e342ac7ee3bc17a6a8e51a043
mamark90 <mamark90@gmail.com>
2021-07-31 21:03:34 -0400

    commit tree efdc4829cdc79dcc2bcd3014e0cf68ca5523b56b

--- src/index.js
+++ src/index.js
@@ -1,105 +1,142 @@
-let addToy = false;
-const newToyForm = document.querySelector('form.add-toy-form')
-const toyCollectionDiv = document.querySelector('div#toy-collection')
+//get object from db.json and iterate over the object to populate characters
+document.addEventListener("DOMContentLoaded", function() {
+});
+const toyCollection = document.getElementById("toy-collection");
+const addNewToyButton = document.getElementById("new-toy-btn")
+const createNewToyButton = document.querySelector(".submit")
 
 
-document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
-    // hide & seek with the form
-    addToy = !addToy;
-    if (addToy) {
-      toyFormContainer.style.display = "block";
-    } else {
-      toyFormContainer.style.display = "none";
-    }
-  });
+addNewToyButton.addEventListener("click", function() {
+  showInputForm();
+})
+toyCollection.addEventListener("click", function(e) {
+  addToLikeCounter(e);
+})
+createNewToyButton.addEventListener("click", function() {
+  addCharacter();
 });
 
 
-function renderOneToy(toy) {
-  const div = document.createElement('div')
-  div.classList.add('card')
-  div.dataset.id = toy.id
 
-  div.innerHTML = `
-          <h2>${toy.name}</h2>
-          <img src=${toy.image} class="toy-avatar" />
-          <p>${toy.likes} Likes </p>
-          <button class="like-btn" data-id=${toy.id}>Like <3</button>`
+getCharacters();
 
+function getCharacters() {
+  fetch("http://localhost:3000/toys")
+  .then(response => response.json()) 
+  .then(data => populateCards(data))
+}
 
-  const toyCollectionDiv = document.querySelector('div#toy-collection')
-  toyCollectionDiv.append(div)
+ 
+function populateCards(characters) {
+  let id = document.getElementById("toy-collection")
+    for(const character of characters) {
+	  let div = document.createElement("div")
+	    div.className = "card";
+        id.appendChild(div)
+		div.dataset.id = character.id;
+		let h2 = document.createElement("h2");
+		h2.innerHTML = character.name;
+		let img = document.createElement("img");
+		img.className = "toy-avatar"
+		img.src = character.image;
+		let p = document.createElement("p");
+		p.innerHTML = character.likes; 
+        p.innerHTML += character.likes > 1 ? " likes" : " like"
+		
+		const btn = document.createElement("BUTTON");
+		btn.className = "like-btn"
+		btn.innerHTML= "Like <3"
+		div.appendChild(h2);
+		div.appendChild(img);
+		div.appendChild(p);
+		div.appendChild(btn);
+	}
+
 }
 
 
-function renderAllToys() {
-  fetch('http://localhost:3000/toys')
-    .then(response => {
-      // console.log(response)
-      return response.json()
+function addToLikeCounter(event) {
+	let e = event["path"][1]
+	  let p = e.querySelector("p");
+	  let pageLikes = p.innerHTML;
+	  let updatedPageLikes = parseInt(pageLikes) + 1;
+	  p.innerHTML = updatedPageLikes.toString();
+	  p.innerHTML += parseInt(p.innerHTML) > 1 ? " likes" : " like"
+	  updateChar(event); 
+}   
+
+
+
+function updateChar(e) {
+  const charId = e.path[1].dataset.id
+  let element = e.path[1]
+  let p = element.querySelector("p");
+  const num = parseInt(p.innerText).toString();
+
+
+  fetch(`http://localhost:3000/toys/${charId}`,  {
+        
+     method: "PATCH",
+     headers: {
+         "Content-Type": "application/json",
+         "Accept": "application/json"
+         },
+
+        body: JSON.stringify({likes: num})
+   		
+   		})
+    .then(data => {
+        console.log(data);
     })
-    .then(toyArr => toyArr.forEach(renderOneToy))
+    .catch(function(error){
+        document.body.innerHTML = error.message
+    })
+}
+
+
+function showInputForm() {
+  const formData = {};
+  var con = document.querySelector(".container");
+
+  if (con.style.display === "") {
+     con.style.display = 'block';
+   } else {
+     con.style.display = "";
+   }
 }
 
 
 
+function formData() {	
+   data	= {};
+   data["name"] = document.body.getElementsByTagName("input")[0].value;
+   data["image"] = document.body.getElementsByTagName("input")[1].value;
+   data["likes"] = 0
+   return data;
+}
 
 
-newToyForm.addEventListener('submit', e => {
-  e.preventDefault()
-  // console.log(e.target)
-  // debugger
 
-  const nameInput = e.target.name.value
-  const imageInput = e.target.image.value
+function addCharacter() {
+  fetch("http://localhost:3000/toys",  {
+        
+     method: "POST",
+     headers: {
+         "Content-Type": "application/json",
+         "Accept": "application/json"
+         },
 
-  const configObject = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json'
-    },
-    body: JSON.stringify({ name: nameInput, image: imageInput, likes: 0 })
-  }
-
-  fetch('http://localhost:3000/toys', configObject)
-    .then(response => {
-      console.log(response)
-      return response.json()
+        body: JSON.stringify(formData())
+   		
+   		})
+    .then(data => {
+        console.log(data);
     })
-    .then(toy => {
-      renderOneToy(toy)
+    .catch(function(error){
+        document.body.innerHTML = error.message
     })
-
-})
-
-
-toyCollectionDiv.addEventListener('click', e => {
-  if (e.target.classList.contains('like-btn')) {
-    const pLikesDisplay = e.target.previousElementSibling
-    const oldLikes = parseInt(pLikesDisplay.textContent)
-    const newLikes = oldLikes + 1
-
-    const configObject = {
-      method: "PATCH",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ likes: newLikes })
-    }
-
-    fetch(`http://localhost:3000/toys/${e.target.dataset.id}`, configObject)
-      .then(response => response.json())
-      .then(data => {
-        pLikesDisplay.textContent = `${data.likes} Likes`
-      })
-  }
-})
+    getCharacters();
+}
 
 
 
-/* APP INIT */
-renderAllToys()
